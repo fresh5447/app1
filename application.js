b3f49afc7558d8c35a0b4d226561408626220a88
Doug Walter <doug@kosmojo.com>
2014-08-27 15:27:24 -0700

    Update application.js

--- application.js
+++ application.js
@@ -1,14 +1,4 @@
-$(function(){
-     console.log("Document Ready");
-    $('.restaurant').on("click",function(){
-       // var items = $(".items");
-       // items.filter("[data-source=" + this.id + "]").show();
-       // items.filter("[data-source!=" + this.id + "]").hide();
-        $(".item").hide().filter("[data-source=" + this.id + "]").show();
-
-    })
-    })
-
+//dynamically create JSON object
 var  r = [
     {
         id: "one",
@@ -49,54 +39,93 @@ var  r = [
 ];
 
 
-var restaraunts = "", menuItems="";
+//show menu items on click of restaurant
+$(function(){
+     console.log("Document Ready");
+    $('.restaurant').on("click",function(){
+        //ev.preventDefault();
+       // var items = $(".items");
+       // items.filter("[data-source=" + this.id + "]").show();
+       // items.filter("[data-source!=" + this.id + "]").hide();
+        $(".item").hide().filter("[data-source=" + this.id + "]").show();
 
-r.forEach(function(r,i){
+    });
+
+
+
+
+
+
+
+
+
+
+//Create restaurants and menu items dynamically
+
+
+    var restaraunts = "", menuItems="";
+
+    r.forEach(function(r,i){
         restaraunts += "<li id='" + r.id + "' class='restaurant'> <a href='#'>" + r.name + "</a></li>";
 
         r.menu.forEach(function(m, i){
 
-            menuItems += "<li class='item' data-source='" + r.id + "'> <input type='checkbox'/>"  +  m.title +"Price: $"+ m.price +  "</li>";
-            var menuItemPrice = m.price;
+            menuItems += "<li class='item' data-source='" + r.id + "'> <input type='checkbox' value='" + m.price + "'/>"  +  m.title +"Price: $"+ m.price +  "</li>";
 
-    })
- })
+
+
+        });
+    });
     $("ul#restarauntList").append(restaraunts);
     $("ul#menuList").append(menuItems);
 
+
+
+
 //This is the keystroke counter
-$("#notes").on("keypress", function(){
-    $('#notesCounter').text($(this).val().length);
-    $('#notes').css("background-color", this.value.length > 10 ? "#ff7777" : "fff")
-})
+    $("#notes").on("keyup", function(){
+        $('#notesCounter').text($(this).val().length);
+        $('#notes').css("background-color", this.value.length > 10 ? "#ff7777" : "fff")
+    });
 
 
 
-total = 0;
 
+    var total = 0;
 //Checkbox still not working
 //displays value of Nan
 //need to pass it price from menuItem
+    $(".total").text("Your total is " + total + "!!");
+
+
+
+
+
 
     $(":checkbox").on("change", function() {
-        if (this.checked && !this.hidden){
-            total+=parseInt(this.value);
-            console.log(total);
-            $('.moneyValue').text(" Your total is $" + total);
-        } else {
-            console.log("unchecked");
-            total -= parseInt(this.value);
-            $(".moneyValue").text(" Your total is $" + total);
+            if (this.checked && !this.hidden){
+                total+=parseInt(this.value);
+                console.log(total);
+                $('.total').text(" Your total is $" + total);
+            } else {
+                console.log("unchecked");
+                total -= parseInt(this.value);
+                $(".total").text(" Your total is $" + total);
+            }
         }
-}
-);
+    );
+
+
+
+
+
 
 
 
 
 //Example of using an event for an element that may not exist yet.
 //$('body').on("click", 'h1' ,function(){
-  //  console.log(this);
+    //  console.log(this);
 //})
 
 
@@ -108,5 +137,15 @@ total = 0;
 //to access the text that you typed you need .value, change event, and length
 // .val = keyCount
 //change(key
+
+
+
+
+
+
+
+
+
+});
 
 
